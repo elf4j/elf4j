ee330f65d69acc86e376db5ce1bdf5e41afeaa13
q3769 <wqt.net@gmail.com>
2023-01-15 15:13:30 -0600

    original year

--- src/main/java/elf4j/spi/LoggerFactory.java
+++ src/main/java/elf4j/spi/LoggerFactory.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2023 ELF4J
+ * Copyright (c) 2022 ELF4J
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
