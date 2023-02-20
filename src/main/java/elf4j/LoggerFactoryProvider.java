7841a22e7266d0f48222262019a36f46a352ac1b
q3769 <wqt.net@gmail.com>
2023-02-20 11:38:09 -0600

    code readability

--- src/main/java/elf4j/LoggerFactoryProvider.java
+++ src/main/java/elf4j/LoggerFactoryProvider.java
@@ -33,8 +33,8 @@ import java.util.Optional;
 import java.util.ServiceLoader;
 
 /**
- * Provisions one single ELF4J logging provider for the client application - either the properly configured or, in case
- * of setup errors, the default no-op logging provider.
+ * Provisions one single ELF4J logging provider for the client application - either the properly configured or the
+ * default no-op logging provider.
  */
 enum LoggerFactoryProvider {
     /**
